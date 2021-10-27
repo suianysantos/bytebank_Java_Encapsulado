59a030f555b24ad1b5f650205565b3b0db28635f
suianysantos <suiany.k4@gmail.com>
2021-10-27 11:25:31 -0300

    javaprojeto

---
+++ src/TestaGetESet.java
@@ -0,0 +1,19 @@
+
+public class TestaGetESet {
+
+	public static void main(String[] args) {
+	Conta conta = new Conta(1337, 24226);
+	System.out.println(conta.getNumero());
+	
+	Cliente paulo = new Cliente();
+	paulo.setNome("Paulo Silveira");
+	conta.setTitular(paulo);
+	System.out.println(conta.getTitular().getNome());
+
+	conta.getTitular().setProfissao("programador");
+	System.out.println(conta.getTitular().getNome());
+
+	
+}
+}
+
